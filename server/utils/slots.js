d425265ba9a427663f550df17e47b6f1f5dae217
Epistemopholic <front8nd@gmail.com>
2024-11-21 12:25:39 +0500

    fixed issue in generating timezone based appointment slots

--- server/utils/slots.js
+++ server/utils/slots.js
@@ -1,8 +1,9 @@
 function generateTimeSlots(appointmentConfig) {
   const slots = [];
+  const todayDate = new Date();
   // Convert the current time to Pakistan timezone
   const now = new Date(
-    now.toLocaleString("en-US", { timeZone: "Asia/Karachi" })
+    todayDate.toLocaleString("en-US", { timeZone: "Asia/Karachi" })
   );
   const currentHour = now.getHours();
   const currentMinute = now.getMinutes();
